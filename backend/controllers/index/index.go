9f8ebe9927e66e850327cf02f7778f2794c34959
alireza <a.khorasany@gmail.com>
2021-08-04 20:32:47 +0430

    fix: fix content type header

--- backend/controllers/index/index.go
+++ backend/controllers/index/index.go
@@ -7,6 +7,6 @@ import (
 
 func GetConnectedMessage(w http.ResponseWriter, r *http.Request) {
 	_ = mux.Vars(r)
-	w.Header().Set("Content-Type", "html/text")
+	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
 	w.Write([]byte("ICoffee service started successfully..."))
 }
